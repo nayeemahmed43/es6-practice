15f22b22423c75fc3e659ecebe76cf94c779aec3
unknown <you@example.com>
2020-03-03 12:48:44 +0600

    template

---
+++ template.js
@@ -0,0 +1,9 @@
+const firstName = 'Justin';
+const lastName = 'TimberLake';
+const fullName = firstName + lastName;
+const fullName2 = `${firstName} ${lastName} is a good boy`;
+console.log(fullName2);
+const multiLine = `lorem ipsum dolor set amet 
+lorem ipsum dolor set amet
+lorem ipsum dolor set amet`
+console.log(multiLine);
